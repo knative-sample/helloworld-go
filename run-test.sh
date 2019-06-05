47fb82067d8aa11bd30abd89933b17b819f2d1bc
kubeway <kubeway@gmail.com>
2019-06-05 11:08:29 +0800

    first commit

---
+++ run-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#****************************************************************#
+# Create Date: 2019-02-13 10:15
+#********************************* ******************************#
+
+SVC_NAME="helloworld-go"
+
+export INGRESSGATEWAY=istio-ingressgateway
+export IP_ADDRESS=$(kubectl get svc $INGRESSGATEWAY --namespace istio-system --output jsonpath="{.status.loadBalancer.ingress[*]['ip']}")
+echo "IP_ADDRESS: ${IP_ADDRESS}"
+
+export GATEWAY_IP=`kubectl get svc $INGRESSGATEWAY --namespace istio-system --output jsonpath="{.status.loadBalancer.ingress[*]['ip']}"`
+export DOMAIN_NAME=`kubectl get route ${SVC_NAME} --output jsonpath="{.status.domain}"`
+
+kubectl get ksvc ${SVC_NAME} --output=custom-columns=NAME:.metadata.name,DOMAIN:.status.domain
+
+time curl -H "Host: ${DOMAIN_NAME}" http://${IP_ADDRESS} -v
