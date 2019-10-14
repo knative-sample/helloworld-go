a842a666eec2e44211908ed23252d91735098579
qiulin.nql <qiulin.nql@alibaba-inc.com>
2019-10-14 14:22:47 +0800

    feat(test): test sh

--- sample/run-test.sh
+++ sample/run-test.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 SVC_NAME="helloworld-go"
-
 export INGRESSGATEWAY=istio-ingressgateway
-export IP_ADDRESS=$(kubectl get svc $INGRESSGATEWAY --namespace istio-system --output jsonpath="{.status.loadBalancer.ingress[*]['ip']}")
-echo "IP_ADDRESS: ${IP_ADDRESS}"
-
 export GATEWAY_IP=`kubectl get svc $INGRESSGATEWAY --namespace istio-system --output jsonpath="{.status.loadBalancer.ingress[*]['ip']}"`
-export DOMAIN_NAME=`kubectl get route ${SVC_NAME} --output jsonpath="{.status.domain}"`
+export DOMAIN_NAME=`kubectl get route ${SVC_NAME} --output jsonpath="{.status.url}"| awk -F/ '{print $3}'`
 
-kubectl get ksvc ${SVC_NAME} --output=custom-columns=NAME:.metadata.name,DOMAIN:.status.domain
-
-time curl -H "Host: ${DOMAIN_NAME}" http://${IP_ADDRESS} -v
+time curl -H "Host: ${DOMAIN_NAME}" http://${GATEWAY_IP} -v
